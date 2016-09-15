ff4873713c7b7350d2b481e502d33f23b989454b
praveenearle <praveen.earle@gmail.com>
2016-09-15 15:33:36 +0530

    angular js s1a2
    angular js assign 2 session 1

---
+++ angular js s1a2/angular js s1a2/scripts/script.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+    myApp.controller("myController", function ($scope) {
+        $scope.firstname = "first name is: praveen";
+        $scope.lastname = "last name is : kumar";
+
+    }); 
+
